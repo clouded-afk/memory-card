69820b9ce22141d4870a41d8ec7ebdaf6c9497b4
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-18 17:17:55 -0500

    feat: setup memory card component structure

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+export function MemoryCard({
+    name, image
+}) {
+    return (
+        <div className="memory-card">
+            <img className="card-image" src={image} alt={name} />
+            <div className="card-text">{name}</div>
+        </div>
+    )
+}
