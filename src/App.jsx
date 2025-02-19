a50fd3a32a171a04cfadefe16f49394b6500b341
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-19 16:36:32 -0500

    feat: create function to pull data from pokemon API and display a single random pokemon sprite and name on the memory card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import { useEffect, useState } from 'react'
 import { Header } from './components/Header'
+import { Gameboard } from './components/Gameboard'
+import { MemoryCard } from './components/Card'
 import { Footer } from './components/Footer'
 import './App.css'
 
 function App() {
+  const [pokemonData, setPokemonData] = useState({})
+  const randomID = Math.floor(Math.random() * 1025) + 1
+  const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/"
+
+  useEffect(() => {
+    async function getPokemonData(pokemonID) {
+      try {
+        const response = await fetch(`${POKEMON_URL}${pokemonID}`)
+        const data = await response.json()
+        const extractedData = {
+          name: data.name,
+          image: data.sprites.front_default
+        }
+        setPokemonData(extractedData)
+      } catch (error) {
+        console.error('Error fetching data', error)
+      }
+    }
+    getPokemonData(randomID)
+  }, [])  
 
   return (
-    <>
+    <div className='main-container'>
       <Header />
+      <MemoryCard name={pokemonData.name} image={pokemonData.image}/>
       <Footer />
-    </>
+    </div>
   )
 }
 export default App
