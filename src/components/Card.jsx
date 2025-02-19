d47c0753217f7af13c352a6bdc8536f050ae454d
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-19 16:31:17 -0500

    chore: import stylesheet for componenent

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,11 @@
+import '../styles/Card.css'
+
 export function MemoryCard({
     name, image
 }) {
     return (
         <div className="memory-card">
-            <img className="card-image" src={image} alt={name} />
+            <img className="card-image" src={image} alt={name}/>
             <div className="card-text">{name}</div>
         </div>
     )
