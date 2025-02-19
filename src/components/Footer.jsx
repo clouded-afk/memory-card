3726d35ea45fcc3a383b80b84bb76e7a790d1e63
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-18 20:01:32 -0500

    style: add preliminary styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import '../styles/Footer.css'
 
 export function Footer() {
     return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import '../styles/Header.css'
+
 export function Header() {
     return (
         <div className="header">
