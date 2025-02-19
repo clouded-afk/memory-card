86a95f4d5fb78b0569fa47f54dcff5a7135a6a02
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-19 18:01:11 -0500

    feat: import Memory Card component and pass through data from the API to the element

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,9 +1,13 @@
+import { MemoryCard } from './Card'
 import '../styles/Gameboard.css'
 
-export function Gameboard() {
+export function Gameboard({
+    name,
+    image
+}) {
     return (
         <div className="gameboard-container">
-
+            <MemoryCard name={name} image={image} />
         </div>
     )
 }
