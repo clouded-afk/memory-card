0646e03f9a126f849e7237cb8ccc82af10e767dc
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-20 18:45:58 -0500

    chore: add prop for passing of clicking function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
 import '../styles/Card.css'
 
 export function MemoryCard({
-    name, image
+    name, 
+    image,
+    onClick
 }) {
     return (
-        <div className="memory-card">
+        <div className="memory-card" onClick={onClick}>
             <img className="card-image" src={image} alt={name}/>
             <div className="card-text">{name}</div>
         </div>
