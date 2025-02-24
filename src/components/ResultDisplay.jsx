20a4dde1a341d61cb77278e369bbaaf9f10e3e63
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-24 18:05:37 -0500

    chore: pass handlePlayAgain to onClick of play again button

--- src/components/ResultDisplay.jsx
+++ src/components/ResultDisplay.jsx
@@ -1,14 +1,15 @@
 import '../styles/ResultDisplay.css'
 
 export function ResultDisplay({
-    score
+    score,
+    handlePlayAgain
 }) {
     return (
         <div className='content-blocker'>
             <div className="result-container">
                 <h1 className="result-text">Game Over!</h1>
                 <div className="result-score">Score: {score}</div>
-                <button className="play-again">Play Again?</button>
+                <button className="play-again" onClick={handlePlayAgain}>Play Again?</button>
             </div>
         </div>
     )
