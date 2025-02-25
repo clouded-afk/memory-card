68cd4dfa33a8e00766ea6f5313eac7fbf81c258f
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-02-25 16:08:24 -0500

    fix: edit structure of Header component for ease of styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,16 @@ import '../styles/Header.css'
 export function Header() {
     return (
         <div className="header">
-            <h1>Pokemon Memory Game</h1>
-            <ul>Rules:
-                <li>Click on a Pokemon to score a Point!</li>
-                <li>Click on a new Pokemon after each shuffle to keep increasing you score!</li>
-                <li>When you click a Pokemon that you have clicked before the game ends, Good Luck!</li>
-            </ul>
+            <h1 className='page-header'>Pokemon Memory Game</h1>
+            <div className='rules-container'>
+                <h2 className='rules-header'>Rules</h2>
+                <div className='line-divider'></div>
+                <ul>
+                    <li>Click on a Pokemon to score a Point!</li>
+                    <li>Click on a new Pokemon after each shuffle to keep increasing you score!</li>
+                    <li>When you click a Pokemon that you have clicked before the game ends, Good Luck!</li>
+                </ul>
+            </div>
         </div>
     )
 }
